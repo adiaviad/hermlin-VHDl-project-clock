e6871f236cad65f8139432681cbfdf9c5a1208ab
adiaviad <adaviad4@gmail.com>
2023-02-18 13:17:55 +0200

    ready for test

--- VHDl/VGA_Controller.vhd
+++ VHDl/VGA_Controller.vhd
@@ -6,37 +6,21 @@ entity VGA_Controller is
 port(
 	pixel_column : in std_logic_vector(9 downto 0);
 	pixel_row : in std_logic_vector(9 downto 0);
+	digit1,digit2,digit3,digit4 : in std_logic_vector(3 downto 0);
+	i,j: in std_logic_vector(9 downto 0);
 	letter : out std_logic_vector(5 downto 0));
+	
 end entity;
 
 architecture arch_VGA_Controller of VGA_Controller is
 begin
-		letter<=conv_std_logic_vector(8,6) when pixel_column>=80 and pixel_column<88 and pixel_row>=240 and pixel_row<248 else
-conv_std_logic_vector(5,6) when pixel_column>=88 and pixel_column<96 and pixel_row>=240 and pixel_row<248 else
-conv_std_logic_vector(12,6) when pixel_column>=96 and pixel_column<104 and pixel_row>=240 and pixel_row<248 else
-conv_std_logic_vector(12,6) when pixel_column>=104 and pixel_column<112 and pixel_row>=240 and pixel_row<248 else
-conv_std_logic_vector(15,6) when pixel_column>=112 and pixel_column<120 and pixel_row>=240 and pixel_row<248 else
-conv_std_logic_vector(32,6) when pixel_column>=120 and pixel_column<128 and pixel_row>=240 and pixel_row<248 else
-conv_std_logic_vector(7,6) when pixel_column>=128 and pixel_column<136 and pixel_row>=240 and pixel_row<248 else
-conv_std_logic_vector(18,6) when pixel_column>=136 and pixel_column<144 and pixel_row>=240 and pixel_row<248 else
-conv_std_logic_vector(5,6) when pixel_column>=144 and pixel_column<152 and pixel_row>=240 and pixel_row<248 else
-conv_std_logic_vector(7,6) when pixel_column>=152 and pixel_column<160 and pixel_row>=240 and pixel_row<248 else
-conv_std_logic_vector(-52,6) when pixel_column>=160 and pixel_column<168 and pixel_row>=240 and pixel_row<248 else
-conv_std_logic_vector(32,6) when pixel_column>=168 and pixel_column<176 and pixel_row>=240 and pixel_row<248 else
-conv_std_logic_vector(8,6) when pixel_column>=176 and pixel_column<184 and pixel_row>=240 and pixel_row<248 else
-conv_std_logic_vector(5,6) when pixel_column>=184 and pixel_column<192 and pixel_row>=240 and pixel_row<248 else
-conv_std_logic_vector(12,6) when pixel_column>=192 and pixel_column<200 and pixel_row>=240 and pixel_row<248 else
-conv_std_logic_vector(12,6) when pixel_column>=200 and pixel_column<208 and pixel_row>=240 and pixel_row<248 else
-conv_std_logic_vector(15,6) when pixel_column>=208 and pixel_column<216 and pixel_row>=240 and pixel_row<248 else
-conv_std_logic_vector(32,6) when pixel_column>=216 and pixel_column<224 and pixel_row>=240 and pixel_row<248 else
-conv_std_logic_vector(4,6) when pixel_column>=224 and pixel_column<232 and pixel_row>=240 and pixel_row<248 else
-conv_std_logic_vector(1,6) when pixel_column>=232 and pixel_column<240 and pixel_row>=240 and pixel_row<248 else
-conv_std_logic_vector(14,6) when pixel_column>=240 and pixel_column<248 and pixel_row>=240 and pixel_row<248 else
-conv_std_logic_vector(9,6) when pixel_column>=248 and pixel_column<256 and pixel_row>=240 and pixel_row<248 else
-conv_std_logic_vector(5,6) when pixel_column>=256 and pixel_column<264 and pixel_row>=240 and pixel_row<248 else
-conv_std_logic_vector(12,6) when pixel_column>=264 and pixel_column<272 and pixel_row>=240 and pixel_row<248 else
-conv_std_logic_vector(32,6) when pixel_column>=272 and pixel_column<280 and pixel_row>=240 and pixel_row<248 else
- "100000";
+		letter<=
+		   (digit1 xor conv_std_logic_vector(48,6)) when pixel_column>=(0+i) and pixel_column<(8+i)  and pixel_row>=(0+j) and pixel_row<(8+j) else
+			(digit2 xor conv_std_logic_vector(48,6)) when pixel_column>=(8+i) and pixel_column<(16+i)  and pixel_row>=(0+j) and pixel_row<(8+j) else
+			"111111" when pixel_column>=(16+i) and pixel_column<(24+i)  and pixel_row>=(0+j) and pixel_row<(8+j) else
+			(digit3 xor conv_std_logic_vector(48,6)) when pixel_column>=(24+i) and pixel_column<(32+i)  and pixel_row>=(0+j) and pixel_row<(8+j) else
+			(digit4 xor conv_std_logic_vector(48,6)) when pixel_column>=(32+i) and pixel_column<(40+i)  and pixel_row>=(0+j) and pixel_row<(8+j) else
+			"100000";
 end architecture;
 
 		  
